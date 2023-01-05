e66232ac65202f565ffd53754c046eccf5b176bd
Irina Scheglova <120469546+I-rina1912@users.noreply.github.com>
2023-01-05 21:08:08 +0300

    Add files via upload

---
+++ Ekz_3.py
@@ -0,0 +1,29 @@
+# Задача 1.
+# Напишите функцию, которая будет принимать номер кредитной карты и
+# показывать только последние 4 цифры. Остальные цифры
+# должны заменяться звездочками
+# def karta(card='1234567891239567'):
+#     new_card = '*' * 12 + card[-4:]
+#     print(new_card)
+#
+#
+# karta()
+
+
+
+# Задача 2.
+# Напишите функцию, которая проверяет: является ли слово палиндромом
+
+# stroka = input('Введите слово: ')
+#
+#
+# def check_palindrome(stroka):
+#     reversed_stroka = stroka[::-1]
+#
+#     if stroka == reversed_stroka:
+#         print(stroka, "is a Palindrome")
+#     else:
+#         print(stroka, "is not a Palindrome")
+#
+#
+# check_palindrome(stroka)
